db111212e2cda0323387f3bcfd4bff05f545326e
NineSongs <40531423+Nine-Songs@users.noreply.github.com>
2018-06-27 15:09:37 +0800

    Create 二叉搜索函数.py
    二叉搜索只针对与有顺序的列表，先选定中间值进行比较，如果目标比中间值大则在中间值的右边找，如果目标比中间值小则在中间值的左边找，依次找下去为止。
    // 为向下取整除法

---
+++ 03/二叉搜索函数.py
@@ -0,0 +1,15 @@
+'''
+Two-fork search function with sequence table
+'''
+def binarySearch(target,lyst):
+	left = 0
+	right = len(lyst)-1
+	while left <= right :
+		midpoint = (left + right) // 2
+		if lyst[midpoint] == target:
+			return midpoint
+		elif lyst[midpoint] < target:
+			right = midpoint - 1
+		elif lyst[midpoint] > target:
+			left = midpoint + 1
+	return -1
